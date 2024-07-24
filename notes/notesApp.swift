7286532616fc9b32511c35c3a6cb9d3149a334cd
josh richards <joshuarichards001@gmail.com>
2024-07-24 20:49:51 +1200

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Josh Richards on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
